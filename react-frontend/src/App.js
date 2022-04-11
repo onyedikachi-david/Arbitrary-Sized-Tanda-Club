c04bfb1bcb03d59d50279309c5e60928e99be434
nwakaku <nwakakucc@gmail.com>
2022-04-11 16:30:45 +0000

    tanda

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import person from './img/person.png'
+import shape from './img/shape.png'
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+      <div className="big-wrapper light">
+        <img src={shape} alt="" className="shape" />
+
+        <header>
+          <div className="container">
+            <div className="logo">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxjdX_-8MxBDQtQMyNsKg7x84MA5a4g71lGKO1wjvqMA&s" alt="Logo" />
+              <h3>Tanda Umoja</h3>
+            </div>
+
+            <div className="links">
+              <ul>
+                <li><a href="#">Features</a></li>
+                <li><a href="#">Pricing</a></li>
+                <li><a href="#">Testimonials</a></li>
+                <li><a href="#" className="btn">Connect</a></li>
+              </ul>
+            </div>
+
+            <div className="overlay"></div>
+
+            <div className="hamburger-menu">
+              <div className="bar"></div>
+            </div>
+          </div>
+        </header>
+
+        <div className="showcase-area">
+          <div className="container">
+            <div className="left">
+              <div className="big-title">
+                <h1>Future is here,</h1>
+                <h1>Start Exploring now.</h1>
+              </div>
+              <p className="text">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Delectus eius distinctio odit, magni magnam qui ex perferendis
+                vitae!
+              </p>
+              <div className="cta">
+                <a href="#" className="btn">Connect Wallet</a>
+              </div>
+            </div>
+
+            <div className="right">
+              <img src={person} alt="Person Image" className="person" />
+            </div>
+          </div>
+        </div>
+
+        <div className="bottom-area">
+          <div className="container">
+            <button className="toggle-btn">
+              <i className="far fa-moon"></i>
+              <i className="far fa-sun"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+    </main>
     </div>
   );
 }
